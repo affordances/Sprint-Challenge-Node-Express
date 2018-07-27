232127e3e9f05f1c9b35a0d11e19e690f411212c
Gabriel Duquette <gabrielduquette@gmail.com>
2018-07-27 11:56:35 -0400

    add endpoint shells

--- api/actionRoutes.js
+++ api/actionRoutes.js
@@ -2,6 +2,24 @@ const express = require('express');
 const router = express.Router();
 const actions = require('../data/helpers/actionModel');
 
+router.get('/', async (req, res) => {
 
+});
+
+router.get('/:id', async (req, res) => {
+
+});
+
+router.post('/', async (req, res) => {
+
+});
+
+router.put('/:id', async (req, res) => {
+
+});
+
+router.delete('/:id', async (req, res) => {
+
+});
 
 module.exports = router;
--- api/projectRoutes.js
+++ api/projectRoutes.js
@@ -2,6 +2,29 @@ const express = require('express');
 const router = express.Router();
 const projects = require('../data/helpers/projectModel');
 
+router.get('/', async (req, res) => {
+
+});
+
+router.get('/:id', async (req, res) => {
+
+});
+
+router.get('/:id/actions', async (req, res) => {
+
+});
+
+router.post('/', async (req, res) => {
+
+});
+
+router.put('/:id', async (req, res) => {
+
+});
+
+router.delete('/:id', async (req, res) => {
+
+});
 
 
 module.exports = router;
